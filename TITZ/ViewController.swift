0cc31eb41bfe4b5e9d698bab5539fbd1cdc229e2
Intesar Alam <Intesar69@intesars-mac.local>
2016-08-29 03:45:32 +0600

    Initial Commit

---
+++ TITZ/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TITZ
+//
+//  Created by Intesar Alam on 8/29/16.
+//  Copyright © 2016 com.NiggasIncorporated. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
